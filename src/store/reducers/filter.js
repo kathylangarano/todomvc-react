dabe08ff94b7e11836576b1d85e498cc327e1cd1
Katherine <kathylangarano@outlook.es>
2025-08-07 22:12:33 -0400

    work3

--- src/store/reducers/filter.js
+++ src/store/reducers/filter.js
@@ -1,5 +1,7 @@
 import { ACTION_TYPES } from '../../constants/action-type';
 import { FILTERS } from '../../constants/filter';
+import { useState } from 'react';
+
 
 export const filterReducer = (state = FILTERS.all, action) => {
   switch (action.type) {
